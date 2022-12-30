f71bbdd56aeb0ce3bbb920fd18eafa557a0c2752
Dhananjay <dmmohokar94@gmail.com>
2022-12-30 17:47:40 +0530

    hibernate listing

--- secondhybernateapp/src/main/java/org/fi/secondhybernateapp/App.java
+++ secondhybernateapp/src/main/java/org/fi/secondhybernateapp/App.java
@@ -1,11 +1,13 @@
 package org.fi.secondhybernateapp;
 
 import java.util.Scanner;
+import java.util.List;
 
 import org.fi.secondhybernateapp.entity.Users;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 /**
  * Hello world!
@@ -21,10 +23,19 @@ public class App
     	    		Session hibernateSession = hibernateFactory.openSession();
     	    		Scanner scanner = new Scanner(System.in))
     	    {
-    	    	System.out.println("Enter the user name");
-    	    	String userName = scanner.next();
-    	    	Users objUsers= hibernateSession.load(Users.class,userName);
-    	    	System.out.println(objUsers);
+				/*
+				 * System.out.println("Enter the user name"); String userName = scanner.next();
+				 * Users objUsers= hibernateSession.load(Users.class,userName);
+				 * System.out.println(objUsers);
+				 */
+    	    	//HQL
+    	    	Query<Users> query= hibernateSession.createQuery("select OBJECT(objUser) from Users objUser");
+    	    	List<Users> list = query.list();
+    	    	for(Users objUser : list)
+    	    	{
+    	    		System.out.println(objUser);
+    	    	}
+    	    	
     	    }
     }
 }
