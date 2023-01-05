3e47b3ebd8755542efaa34c93ef72421e484bf83
Dhananjay <dmmohokar94@gmail.com>
2023-01-06 00:57:45 +0530

    Spring boot using postman

--- secondbootrest/secondbootrest/src/main/java/org/fi/spring/secondbootrest/restful/UserController.java
+++ secondbootrest/secondbootrest/src/main/java/org/fi/spring/secondbootrest/restful/UserController.java
@@ -1,5 +1,6 @@
 package org.fi.spring.secondbootrest.restful;
 
+import org.fi.spring.secondbootrest.dto.UserInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 @GetMapping("/authenticate")
-public boolean processAuthenticate(@RequestParam("userName")String userName,@RequestParam("password")String password)
+public UserInfo processAuthenticate(@RequestParam("userName")String userName,@RequestParam("password")String password)
 {
 	if(userName.equals("dhananjay") && password.equals("cdac"))
-		return true;
+	{
+		UserInfo objUser = new UserInfo();
+		objUser.setUserName("Dhananjay");
+		objUser.setPassword("*******");
+		objUser.setName("Dhananjay Mohokar");
+		objUser.setEmail("dhananjay@mohokar.com");
+	
+		return objUser;
+		}
 	else
-	return false;
+	return null;
 }
 }
